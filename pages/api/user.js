845e3b6df791886db096fac84c736441db59b741
aki <>
2020-07-30 00:51:29 +0900

    add get and post api

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,3 +1,11 @@
 export default (req, res) => {
-  res.status(200).json({ message: `you requested for ${req.query.name} ` });
+  switch(req.method) {
+    case 'GET':
+      res.status(200).json({ message: `you requested for ${req.query.name}` });
+      break;
+    case 'POST':
+      res.status(200).json({ message: `user name is ${req.body['name']}`});
+      break;
+  }
+      
  };
