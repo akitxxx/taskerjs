abac3720f3770708410bdb78f9ea0d156f8d6010
aki <>
2020-08-30 00:02:42 +0900

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -190,8 +190,6 @@ module.exports = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+  presets: ["@babel/preset-env", "@babel/preset-react"],
 };
 
-module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"]
-};
